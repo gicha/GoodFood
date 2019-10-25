dd13f5804a542818a52dbfe722386a444c5da9fb
gicha <01gicha01@gmail.com>
2019-10-26 00:43:49 +0200

    add utils config manifest and gradle files

---
+++ lib/utils/config.dart
@@ -0,0 +1,8 @@
+/// Config file with system variables
+/// {@category Utils}
+class Config {
+  static const String dbName = "goodfood";
+  static const int dbVersion = 1;
+  static String apiUrl = "http://100.98.2.193:8000";
+  static String apiVersion = "";
+}
